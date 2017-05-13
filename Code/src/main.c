74f31418f1139a058deed08768f7ca2ebde314bc
FlorentBerland <florent.berland@laposte.net>
2017-05-13 07:01:40 +0200

    Test serveur position pour le 1/06/2017

--- Code/src/main.c
+++ Code/src/main.c
@@ -6,6 +6,7 @@
 #include "../headers/driver_cptr.h"
 #include "../headers/rectangle.h"
 #include "../headers/gopigo.h"
+#include "../headers/positionnement.h"
 //#include "../headers/com_interface.h"
 
 //Test de l'infrarouge
@@ -14,24 +15,24 @@
 
 int main(int argc, char **argv)
 {	
-	/*
-	init();
-	pinMode(pin_led, "OUTPUT");
-	analogWrite(pin_led, 255);
-	pinMode(pin_capt, "INPUT");
-	int i=0;
-	for(;i<100;i++)
+	while(true)
 	{
-		printf("%d\n", digitalRead(pin_capt));
+		ctrl_robot.rec.centre.x = PositionX();
+		ctrl_robot.rec.centre.y = PositionY();
+		printf("Position du robot : ")		
+		r_afficher_point(ctrl_robot.rec.centre);
+		printf("\n");
 		sleep(1);
 	}
-	*/
 
+/*
 	init();	
 
 	fwd();
 	pi_sleep(5000);
 	stop();
+*/
+
 /*
 	ctrl_robot = REC_ORIGINE;
 	ctrl_distributeur = (r_rect){ 0, 100, 10, 10, 0 };
