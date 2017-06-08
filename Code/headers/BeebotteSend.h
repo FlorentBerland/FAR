ed479e70eba58b6a01749ab0f28ce349fcc511e1
Mspaenle <Mahe.spaenle@etu.umontpellier.fr>
2017-06-08 16:34:57 +0200

    Add files via upload

---
+++ Code/headers/BeebotteSend.h
@@ -0,0 +1,13 @@
+#include <stdio.h> /* printf, sprintf */
+#include <stdlib.h> /* exit, atoi, malloc, free */
+#include <unistd.h> /* read, write, close */
+#include <string.h> /* memcpy, memset */
+#include <sys/socket.h> /* socket, connect */
+#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
+#include <netdb.h> /* struct hostent, gethostbyname */
+#include "../headers/ipipip.h"
+
+void error(const char *) ;
+
+
+int sendToBeBotte(char *, char *, char * char *);
