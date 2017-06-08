d8dc1a6c3c1f073719face5ef5997cc442002143
Mspaenle <Mahe.spaenle@etu.umontpellier.fr>
2017-06-08 16:35:38 +0200

    Create BeebotteSend.c

--- Code/src/BeebotteSend.c
+++ Code/src/BeebotteSend.c
@@ -1,11 +1,4 @@
-#include <stdio.h> /* printf, sprintf */
-#include <stdlib.h> /* exit, atoi, malloc, free */
-#include <unistd.h> /* read, write, close */
-#include <string.h> /* memcpy, memset */
-#include <sys/socket.h> /* socket, connect */
-#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
-#include <netdb.h> /* struct hostent, gethostbyname */
-#include "../headers/ipipip.h"
+#include "../headers/BeebotteSend.h"
 
 void error(const char *msg) { perror(msg); exit(0); }
 
