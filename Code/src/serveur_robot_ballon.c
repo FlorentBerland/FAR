92680a395ff3b6751cda420c52022f9fcaa88380
Mspaenle <Mahe.spaenle@etu.umontpellier.fr>
2017-06-08 15:30:50 +0200

    headers

--- Code/src/serveur_robot_ballon.c
+++ Code/src/serveur_robot_ballon.c
@@ -1,19 +1,5 @@
-#include <sys/socket.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 
-/* Appels et fonctions systeme */
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
-/* Bibliothéques usuelles */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-
+#include "../headers/serveur_robot_ballon.h"
 #define PORT 10002
 
 
